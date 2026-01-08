4a0a34bd4120733f304d8683d20b2fb75a9cb248
redstonerti <redstonerti@gmail.com>
2026-01-08 17:21:55 +0200

    Update ex3.asm

--- ex3.asm
+++ ex3.asm
@@ -1,7 +1,32 @@
 .text
-.globl __start:
 
 __start:
+    li $a0, 8
+    li $v0, 9
+    syscall
+
+    move $t1, $v0
+
+    li $v0, 5
+    syscall
+
+    sw $v0, ($t1)
+
+    li $a0, 8
+    li $v0, 9
+    syscall
+
+    sw $v0, 4($t1)
+    li $v0, 5
+    syscall
+
+    lw $t2, 4($t1)
+    sw $v0, 4($t2)
+
+    sw $a0, 4($t2)
+    li $v0, 1
+    syscall
+
     li $v0, 10
     syscall #woah
 
