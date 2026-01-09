e7cbb50bbc3b9ecb91d009119bca38d60cffb8a3
redstonerti <redstonerti@gmail.com>
2026-01-09 11:30:08 +0200

    Added insertNode and createNode

--- ex3.asm
+++ ex3.asm
@@ -2,40 +2,61 @@
 .globl main
 
 main:
-    jal request_mem
-
-    move $t0, $v0   # $t0 = $v0
-
-    li $v0, 5       # $v0 = readInt()
+    li $a0, 8       # $a0 = 8 => request 8 bytes
+    li $v0, 9
     syscall
 
-    sw $v0, ($t0)   # (first 4 bytes of memory location $t0 is pointing to) = integer read
+    move $s0, $v0   # save memory address of first node to $s0
 
-    jal request_mem
+    # insertNode(78 (data), $s0 (memory address))
+    li $a0, 78
+    move $a1, $s0
+    jal insertNode
 
-    sw $v0, 4($t0)  # (last 4 bytes of memory location $t0 is pointing to) = $v0 = memory location of next node
-
-    li $t1, 9       # $t1 = 9
-    lw $t2, 4($t0)  # $t2 = pointer of first node = address of second node
-    sw $t1, ($t2)   # data of first node = $t1
-
-    la $a0, end_msg
+    la $a0, end_msg # print end_msg
     li $v0, 4
     syscall
 
     li $v0, 10      # exit
     syscall
-
-request_mem:
+insertNode:
     sub $sp, $sp, 4
     sw $ra, ($sp)
+    #arg $a0 = int data value
+    #arg $a1 = memory address of first node
 
+    move $t4, $a1
+
+    jal createNode
+loop:
+   lw $t6, 4($t4)
+   beq $zero, $t6, exit_loop
+   lw $t4, 4($t4)
+   j loop
+exit_loop:
+    sw $v0, 4($t4)
+
+    move $v0, $a1
+
+    #return value $v0 = memory address of first node
+    lw $ra, ($sp)
+    add $sp, $sp, 4
+    jr $ra
+createNode:
+    sub $sp, $sp, 4
+    sw $ra, ($sp)
+    #arg $a0 = int data value
+
+    move $t0, $a0
+    #request memory
     li $a0, 8       # $a0 = 8 => request 8 bytes
     li $v0, 9
-    syscall         # $v0 = memory address of the space requested
+    syscall
 
-    j return        # go to return address
-return:
+    sw $t0, ($v0)         #(first 4 bytes of memory requested) = int argument
+    sw $zero, 4($v0)      #(last 4 bytes of memory requested) = 0 (terminator)
+
+    #return value $v0 = memory address of node
     lw $ra, ($sp)
     add $sp, $sp, 4
     jr $ra
